8f4b2b31bd5c97ffed569617542e9e49c832a824
akram <akram.khan200@gmail.com>
2026-01-10 13:52:51 +0530

    tools set up 1.1.234

--- module/main.tf
+++ module/main.tf
@@ -36,7 +36,7 @@ resource "aws_iam_instance_profile" "prometheus_profile" {
 
 
 resource "aws_route53_record" "internal-record" {
-  name    = var.tool_name
+  name    = "${var.tool_name}-internal"
   type    = "A"
   zone_id = "${var.zone_id}"
   records = [aws_instance.instance.private_ip]
