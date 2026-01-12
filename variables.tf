af5633b9acf1e8376f228c042e2212abe754c694
akram <akram.khan200@gmail.com>
2026-01-12 16:14:47 +0530

    tools set up 1.1.234

--- variables.tf
+++ variables.tf
@@ -6,6 +6,12 @@ variable "tools" {
       policy_resource_list = ["ec2:DescribeInstances"]
       instance_type = "t3.small"
     }
+
+      grafana = {
+        instance_type = "t3.small"
+        policy_resource_list = []
+      }
+
   }
 }
 
