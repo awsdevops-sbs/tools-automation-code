8e46ab66e8f0498f891b0e363f946f7bc8a546fe
akram <akram.khan200@gmail.com>
2026-02-12 01:38:23 +0530

    Revert "elastic serach"
    This reverts commit aac931b279ff54a2906ae6d936a0d280d3862a8b.

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "4.5.0"
+    }
+  }
+}
+# provider "vault" {
+#   address = "https://vault.awsdevops16297.sbs:8200/"
+#   skip_tls_verify = true
+#   token = var.vault_token
+# }
