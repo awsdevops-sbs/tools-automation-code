a568f88dfeeb5c014180ab4448af69549bd272d8
akram <akram.khan200@gmail.com>
2026-04-18 15:31:52 +0530

    Artifactory changes

--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "tools" {
         policy_resource_list = []
       }
 
-      Artifactory = {
+      artifactory = {
         instance_type = "t3.large"
         policy_resource_list = []
       }
